8c18c98f91ef5072b116e5a7e2340488d9d3ea9a
Suziwan <christe.suzanne@gmail.com>
2023-01-25 16:32:05 +0000

    Add tests

--- lib/first_tweet.rb
+++ lib/first_tweet.rb
@@ -2,13 +2,18 @@
 require 'twitter'
 
 # N'oublie pas les lignes pour Dotenv ici…
+require 'dotenv'
+Dotenv.load
   
 # Quelques lignes qui appellent les clés d'API de ton fichier .env
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  #config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    #config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  return client
 end
 
 # Ligne qui permet de tweeter sur ton compte
